d2510dea2c826c8a8f9d1ce948f33c824339d9f6
Jose Junior <juniorferreira1919@hotmail.com>
2024-01-05 19:07:23 -0300

    feat: add fundamentos sql

---
+++ Fundamentos-Sql/CreateBlog.sql
@@ -0,0 +1,91 @@
+CREATE DATABASE [Blog]
+GO
+
+USE [Blog]
+GO
+
+
+CREATE TABLE [User](
+    [Id] INT NOT NULL IDENTITY(1,1),
+    [Name] NVARCHAR(80) NOT NULL,
+    [Email] NVARCHAR(200) NOT NULL,
+    [Password] VARCHAR(255) NOT NULL,
+    [Bio] TEXT NOT NULL,
+    [Image] VARCHAR(2000) NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+
+    CONSTRAINT [Pk_User] PRIMARY KEY([Id]),
+    CONSTRAINT [UQ_User_Email] UNIQUE([Email]),
+    CONSTRAINT [UQ_User_Slug] UNIQUE(Slug)
+)
+CREATE NONCLUSTERED INDEX [IX_User_Email] ON [User]([Email])
+CREATE NONCLUSTERED INDEX [IX_User_Slug] ON [User]([Slug])
+GO
+
+CREATE TABLE [Role](
+    [Id] INT NOT NULL IDENTITY(1,1),
+    [Name] NVARCHAR(80) NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+
+    CONSTRAINT [Pk_Role] PRIMARY KEY([Id]),
+    CONSTRAINT [UQ_Role_Slug] UNIQUE(Slug)
+)
+CREATE NONCLUSTERED INDEX [IX_Role_Slug] ON [Role]([Slug])
+GO
+
+CREATE TABLE [UserRole](
+    [UserId] INT NOT NULL,
+    [RoleId] INT NOT NULL,
+
+    CONSTRAINT [Pk_UserRole] PRIMARY KEY([UserId], [RoleId]),
+)
+GO
+
+CREATE TABLE [Category](
+    [Id] INT NOT NULL IDENTITY(1,1),
+    [Name] NVARCHAR(80) NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+
+    CONSTRAINT [Pk_Category] PRIMARY KEY([Id]),
+    CONSTRAINT [UQ_Category_Slug] UNIQUE(Slug)
+)
+CREATE NONCLUSTERED INDEX [IX_Category_Slug] ON [Category]([Slug])
+GO
+
+CREATE TABLE [Post](
+    [Id] INT NOT NULL IDENTITY(1,1),
+    [CategoryId] INT NOT NULL,
+    [AuthorId] INT NOT NULL,
+    [Title] VARCHAR(160) NOT NULL,
+    [Summary] VARCHAR(255) NOT NULL,
+    [Body] TEXT NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+    [CreateDate] DATETIME NOT NULL DEFAULT(GETDATE()),
+    [LastUpdateDate] DATETIME NOT NULL DEFAULT(GETDATE()),
+
+    CONSTRAINT [Pk_Post] PRIMARY KEY([Id]),
+    CONSTRAINT [FK_Post_Category] FOREIGN KEY([CategoryId]) REFERENCES [Category]([Id]),
+    CONSTRAINT [FK_Post_Author] FOREIGN KEY([AuthorId]) REFERENCES [User]([Id]),
+    CONSTRAINT [UQ_Post_Slug] UNIQUE(Slug)
+)
+CREATE NONCLUSTERED INDEX [IX_Post_Slug] ON [Post]([Slug])
+GO
+
+CREATE TABLE [Tag](
+    [Id] INT NOT NULL IDENTITY(1,1),
+    [Name] NVARCHAR(80) NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+
+    CONSTRAINT [Pk_Tag] PRIMARY KEY([Id]),
+    CONSTRAINT [UQ_Tag_Slug] UNIQUE(Slug)
+)
+CREATE NONCLUSTERED INDEX [IX_Tag_Slug] ON [Tag]([Slug])
+GO
+
+CREATE TABLE [PostTag](
+    [PostId] INT NOT NULL,
+    [TagId] INT NOT NULL,
+
+    CONSTRAINT [Pk_PostTag] PRIMARY KEY([PostId], [TagId]),
+)
+GO
